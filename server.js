5b03bef14cf3a6dbb362316d41e91a489640ec61
Brian Pugh <brian@jaspin.com>
2019-01-17 17:23:24 -0500

    added the projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) =>{
     })
 });
 
+app.get('/projects', (req, res) =>{
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+        portfolioInfo: 'Portfolio'
+    })
+});
+
 app.get('/bad', (req, res) =>{
     res.send({
         error: "404",
